db91218fd0c88b6f0301cb2c8165f00d92624aa5
CHenriqueRibeiro <chmr66@gmail.com>
2024-07-28 01:38:11 -0300

    routes test

--- routes/public.js
+++ routes/public.js
@@ -4,23 +4,32 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 const router = express.Router()
 
+router.get('/', async (req, res) => {
+    try {
+        res.send('PARABENS')
+    } catch (err) {
+        res.status(500).json({ message: 'Erro no servidor' });
+    }
+
+})
 router.post('/cadastro', async (req, res) => {
     try {
         const user = req.body
         const salt = await bcrypt.genSalt(10)
         const hashPassword = await bcrypt.hash(user.password, salt)
-        await prisma.user.create({
+        const userDb = await prisma.user.create({
             data: {
                 name: user.name,
                 email: user.email,
                 password: hashPassword
             }
         })
-        res.status(201).json(user)
+        res.status(201).json(userDb)
     } catch (err) {
         res.status(500).json({ message: 'Erro no servidor' });
     }
 
 })
+
 
 export default router;
